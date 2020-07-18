8eb71d12a648e1e906c1d26b968e9873a65fdf73
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-18 20:43:34 +0000

    Done.

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -1,8 +1,23 @@
 def mario
-  status = 'Thank You Mario! But Our Princess Is In Another Castle!'
+  phrase = 'It\'s-a me, Mario!'
   puts phrase
 end
 
 def toadstool
-  puts status
+  puts 'Thank You Mario! But Our Princess Is In Another Castle!'
+end
+
+def link
+  puts 'It\'s Dangerous To Go Alone! Take This.'
+end
+
+
+
+
+def all_phrases
+
+    puts mario
+    puts link
+    puts toadstool
+
 end
