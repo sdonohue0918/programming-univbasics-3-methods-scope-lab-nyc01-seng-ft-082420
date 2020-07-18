3e2934c7c6b745e8013aa26d21efb265800e5194
observant-mapping-4330 <sdonohue0918@gmail.com>
2020-07-18 20:42:22 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -15,7 +15,7 @@ end
 
 
 def all_phrases(argument)
-  if argument == link || mario || toadstool
+
     puts 'It\'s Dangerous To Go Alone! Take This.'
     puts 'It\'s-a me, Mario!'
     puts 'Thank You Mario! But Our Princess Is In Another Castle!'
